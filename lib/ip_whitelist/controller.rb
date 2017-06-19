26d442db05afad270f06b474b3be8afdc8de2c91
Craig S. Cottingham <craig@cottingham.net>
2017-06-19 15:32:03 -0500

    Add logging of HTTP header names

--- lib/ip_whitelist/controller.rb
+++ lib/ip_whitelist/controller.rb
@@ -3,6 +3,9 @@ module IPWhitelist
     def check_ip_whitelist
       if current_user && current_user.respond_to?(:ip_whitelist) && current_user.ip_whitelist.present?
         # Since requests can come through cloudflare, try that IP first.
+
+        Rails.logger.info("All headers:\n#{request.headers.keys.join("\n")}")
+
         ip = IPAddr.new(request.headers["CF-Connecting-IP"] || request.headers["REMOTE_ADDR"])
 
         return if current_user.ip_whitelist.detect{ |ip_or_range|
