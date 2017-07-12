e52d401817205fb512d56bf815cba9c1acc2f400
Brendan Carney <brendan@onehq.com>
2017-07-12 15:13:53 -0500

    Check cloudflare IP, fallback to request.remote_ip

--- lib/ip_whitelist/controller.rb
+++ lib/ip_whitelist/controller.rb
@@ -1,14 +1,10 @@
 module IPWhitelist
   module Controller
-    POSSIBLE_IP_HEADERS = [
-      "X-Forwarded-For",
-      "CF-Connecting-IP", # Cloudflare
-      "REMOTE_ADDR"
-    ]
-
     def check_ip_whitelist
       if current_user && current_user.respond_to?(:ip_whitelist) && current_user.ip_whitelist.present?
-        ip = IPAddr.new(request.remote_ip)
+        # Since requests can come through cloudflare, try that IP first.
+        ip = IPAddr.new(request.headers["CF-Connecting-IP"] || request.remote_ip)
+
         return if current_user.ip_whitelist.detect{ |ip_or_range|
           # Check if the IP is equal to the whitelisted IP, or is within the
           # whitelisted IP range.
